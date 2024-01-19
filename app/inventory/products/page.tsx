67f1107e35aec562d0856e65c2cb70e3053dad5a
keroken <kenichi.shinagawa@gmail.com>
2024-01-19 15:46:33 +0000

    update product page to load json data

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -1,7 +1,49 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+import productsData from './sample/products.json'
+import styles from './page.module.scss'
+
+type ProductsData = {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+};
+
 export default function Page() {
+    const [data, setData] = useState<Array<ProductsData>>([])
+
+    useEffect(() => {
+        setData(productsData);
+    }, [])
+
     return (
         <>
-            <h2>商品一覧</h2>
+            <h2 className="font-bold text-xl">商品一覧</h2>
+            <button className={styles.button}>商品を追加</button>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>商品ID</th>
+                        <th>商品名</th>
+                        <th>単価</th>
+                        <th>説明</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((data: any) => (
+                        <tr>
+                            <td>{data.id}</td>
+                            <td>{data.name}</td>
+                            <td>{data.price}</td>
+                            <td>{data.description}</td>
+                            <td><button className={styles.button}>更新・削除</button></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             <p>商品の一覧を表示</p>
         </>
     )
