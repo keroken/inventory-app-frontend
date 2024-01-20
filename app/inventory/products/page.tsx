7b137d29f397f10f686f00517161a599549a4382
keroken <kenichi.shinagawa@gmail.com>
2024-01-20 06:18:00 +0000

    delete css module and use spectrum button

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from 'react'
 import productsData from './sample/products.json'
 import Link from 'next/link'
-import styles from './page.module.scss'
+import { Button, defaultTheme, Provider } from '@adobe/react-spectrum' 
 
 type ProductsData = {
     id: number;
@@ -20,10 +20,10 @@ export default function Page() {
     }, [])
 
     return (
-        <>
+        <Provider theme={defaultTheme}>
             <h2 className="font-bold text-xl">商品一覧</h2>
-            <button className={styles.button}>商品を追加</button>
-            <table className={styles.table}>
+            <Button variant="secondary">商品を追加</Button>
+            <table>
                 <thead>
                     <tr>
                         <th>商品ID</th>
@@ -42,12 +42,12 @@ export default function Page() {
                             <td>{data.price}</td>
                             <td>{data.description}</td>
                             <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
-                            <td><button className={styles.button}>更新・削除</button></td>
+                            <td><Button variant="secondary">更新・削除</Button></td>
                         </tr>
                     ))}
                 </tbody>
             </table>
             <p>商品の一覧を表示</p>
-        </>
+        </Provider>
     )
 }
