d08862b93d6496539baee417d6b1d97b32257383
keroken <kenichi.shinagawa@gmail.com>
2024-01-20 12:13:42 +0000

    add styling to product page

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -3,7 +3,8 @@
 import { useEffect, useState } from 'react'
 import productsData from './sample/products.json'
 import Link from 'next/link'
-import { Button, defaultTheme, Provider } from '@adobe/react-spectrum' 
+import { Button, Cell, defaultTheme, Provider } from '@adobe/react-spectrum'
+import { Layout } from '@/app/components/layout'
 
 type ProductsData = {
     id: number;
@@ -19,35 +20,40 @@ export default function Page() {
         setData(productsData);
     }, [])
 
+    const CellClasses = "px-2 py-2 border-b-2 border-slate-400"
+
     return (
         <Provider theme={defaultTheme}>
-            <h2 className="font-bold text-xl">商品一覧</h2>
-            <Button variant="secondary">商品を追加</Button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>商品ID</th>
-                        <th>商品名</th>
-                        <th>単価</th>
-                        <th>説明</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((data: any) => (
+            <Layout>
+                <h2 className="font-bold text-xl pb-6">商品一覧</h2>
+                <div className="pb-6">
+                    <Button variant="secondary">商品を追加</Button>
+                </div>
+                <table>
+                    <thead className="border-b-4 border-sky-500">
                         <tr>
-                            <td>{data.id}</td>
-                            <td>{data.name}</td>
-                            <td>{data.price}</td>
-                            <td>{data.description}</td>
-                            <td><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
-                            <td><Button variant="secondary">更新・削除</Button></td>
+                            <th>商品ID</th>
+                            <th>商品名</th>
+                            <th>単価</th>
+                            <th>説明</th>
+                            <th></th>
+                            <th></th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <p>商品の一覧を表示</p>
+                    </thead>
+                    <tbody>
+                        {data.map((data: any) => (
+                            <tr>
+                                <td className={CellClasses}>{data.id}</td>
+                                <td className={CellClasses}>{data.name}</td>
+                                <td className={CellClasses}>{data.price}</td>
+                                <td className={CellClasses}>{data.description}</td>
+                                <td className={CellClasses}><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
+                                <td className={CellClasses}><Button variant="secondary">更新・削除</Button></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </Layout>
         </Provider>
     )
 }
