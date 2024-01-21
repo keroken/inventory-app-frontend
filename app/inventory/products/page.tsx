335594b85c4cb46a031af0ac35223541c1b6abfa
keroken <kenichi.shinagawa@gmail.com>
2024-01-21 08:39:31 +0000

    add edit and delete ui

--- app/inventory/products/page.tsx
+++ app/inventory/products/page.tsx
@@ -34,6 +34,25 @@ export default function Page() {
     setShowNewRow(false)
   }
 
+  const [editingRow, setEditingRow] = useState(0)
+  const handleEditRow: any = (id: number) => {
+    setShowNewRow(false)
+    setEditingRow(id)
+  }
+
+  const handleEditCancel: any = (id: number) => {
+    setEditingRow(0)
+  }
+
+  const handleEdit: any = (id: number) => {
+    setEditingRow(id)
+  }
+
+  const handleDelete: any = (id: number) => {
+
+    setEditingRow(0)
+  }
+
   const CellClasses = "px-2 py-2 border-b-2 border-slate-400"
 
   return (
@@ -66,14 +85,29 @@ export default function Page() {
               </tr>
             ) : ""}
             {data.map((data: any) => (
-              <tr>
-                <td className={CellClasses}>{data.id}</td>
-                <td className={CellClasses}>{data.name}</td>
-                <td className={CellClasses}>{data.price}</td>
-                <td className={CellClasses}>{data.description}</td>
-                <td className={clsx(CellClasses, 'w-20 text-center')}><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
-                <td className={clsx(CellClasses, 'w-32 text-center')}><Button variant="secondary">更新・削除</Button></td>
-              </tr>
+              editingRow === data.id ? (
+                <tr key={data.id}>
+                  <td className={CellClasses}>{data.id}</td>
+                  <td className={CellClasses}><input type="text" defaultValue={data.name} /></td>
+                  <td className={CellClasses}><input type="number" defaultValue={data.price} /></td>
+                  <td className={CellClasses}><input type="text" defaultValue={data.description} /></td>
+                  <td className={clsx(CellClasses, 'w-20 text-center')}></td>
+                  <td className={clsx(CellClasses, 'w-32 text-center')}>
+                    <button onClick={() => handleEditCancel(data.id)}>キャンセル</button>
+                    <button onClick={() => handleEdit(data.id)}>更新する</button>
+                    <button onClick={() => handleDelete(data.id)}>削除する</button>
+                  </td>
+                </tr>
+              ) : (
+                <tr key={data.id}>
+                  <td className={CellClasses}>{data.id}</td>
+                  <td className={CellClasses}>{data.name}</td>
+                  <td className={CellClasses}>{data.price}</td>
+                  <td className={CellClasses}>{data.description}</td>
+                  <td className={clsx(CellClasses, 'w-20 text-center')}><Link href={`/inventory/products/${data.id}`}>在庫処理</Link></td>
+                  <td className={clsx(CellClasses, 'w-32 text-center')}><button onClick={() => handleEditRow(data.id)}>更新・削除</button></td>
+                </tr>
+              )
             ))}
           </tbody>
         </table>
