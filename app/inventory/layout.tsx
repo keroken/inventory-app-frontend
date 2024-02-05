f687029eb8d0f0d5192cd79b1ef4cb5787aeeb5c
keroken <kenichi.shinagawa@gmail.com>
2024-02-05 15:36:18 +0000

    apply basic layout styling

--- app/inventory/layout.tsx
+++ app/inventory/layout.tsx
@@ -5,16 +5,21 @@ type Props = {
 
 export default function InventoryLayout({ children }: Props) {
   const BaseClasses = 'h-lvh w-full bg-gray-100 py-10 px-12'
+  const HeaderClasses = 'bg-blue-200 p-5'
+  const ContainerClasses = 'grow flex'
+  const NavbarClasses = 'w-{200} bg-blue-100 p-5'
+  const ContentClasses = 'grow-2 p-5'
+  const FooterClasses = 'bg-blue-200 p-5'
   return (
     <div className={BaseClasses}>
-      <header>Header</header>
-      <div>
-        <aside>Sidebar</aside>
-        <main>
+      <header className={HeaderClasses}>Header</header>
+      <div className={ContainerClasses}>
+        <aside className={NavbarClasses}>Sidebar</aside>
+        <main className={ContentClasses}>
           <section>{children}</section>
         </main>
       </div>
-      <footer>Footer</footer>
+      <footer className={FooterClasses}>Footer</footer>
     </div>
   )
 }
